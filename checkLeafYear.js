8308844a03aa5d225f7522de5b07f01838a7bdab
akshaykashid138 <53051596+akshaykashid138@users.noreply.github.com>
2021-02-10 10:03:34 +0530

    Delete checkLeafYear.js

--- checkLeafYear.js
+++
@@ -1,9 +0,0 @@
-var check = function (n) {
-    if (n % 4 == 0 || n % 400 == 0 || n % 100 == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
-console.log(check(2024));
